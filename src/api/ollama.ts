2bec6e87bb60415e0b0ead16c72d3a0e12f32a29
mundanecoder <dipankar.laskar45@gmail.com>
2024-08-07 23:45:01 +0530

    fixed build issue

--- src/api/ollama.ts
+++ src/api/ollama.ts
@@ -1,21 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const ollamaApi = async (input: string) => {
-  const response = await axios.post("https://localhost:11434", input);
-  return response.data;
-};
+// export const ollamaApi = async (input: string) => {
+//   const response = await axios.post("https://localhost:11434", input);
+//   return response.data;
+// };
 
-interface IProps {
-  path: string;
-  className: string[];
-  args: string[];
-}
+// interface IProps {
+//   path: string;
+//   className: string[];
+//   args: string[];
+// }
 
-export const addSparkJob = async ({ path, args, className }: IProps) => {
-  const response = await axios.post("http://16.171.237.145:5000/submit", {
-    path,
-    args,
-    className,
-  });
-  return response.data;
-};
+// export const addSparkJob = async ({ path, args, className }: IProps) => {
+//   const response = await axios.post("http://16.171.237.145:5000/submit", {
+//     path,
+//     args,
+//     className,
+//   });
+//   return response.data;
+// };
