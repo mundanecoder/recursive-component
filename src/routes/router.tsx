4177e3efab50a63e80546e6964affdc4561f04ce
mundanecoder <dipankar.laskar45@gmail.com>
2024-08-08 23:05:21 +0530

    fixed build issue

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
-import App from "../App";
 import FileSystem from "../pages/File/index";
 const router = createBrowserRouter([
   {
